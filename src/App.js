b0c0e020072092a083f4ba4d90109c5126ff17ec
Pierluigi Chiodin <piergiggisc@gmail.com>
2022-11-21 14:58:48 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ciao a tutti belli e brutti !
+          Ciao a tutti belli e brutti !<br/>
+          FORZA MILAN !
         </p>
         <a
           className="App-link"
